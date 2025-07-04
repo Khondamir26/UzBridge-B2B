bb9112b6f9ed0734494dea704a96a5b4a6a4652e
khonda <82398768+Khondamir26@users.noreply.github.com>
2025-07-04 16:19:11 +0500

    terms

---
+++ src/app/cookies/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { useState } from 'react';
+
+const Cookies = () => {
+
+};
+
+export default Cookies;
---
+++ src/app/terms/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { useState } from 'react';
+
+const Terms = () => {
+
+};
+
+export default Terms;
